eb08829e78910a8b30daaf73e74c33bb62fefb25
Amian A. <amiandevsec@gmail.com>
2026-05-26 18:01:32 +0000

    Bump version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ptmap",
-    version="1.0.0",
+    version="1.0.1",
     description="Asynchronous path traversal fuzzer",
     author="AmianDevSec",
     author_email="amiandevsec@gmail.com",
